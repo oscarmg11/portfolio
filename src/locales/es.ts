8b568e2d008d9b1c4a01e3651c2d595c3b13e10f
Oscar Moreno <oscar@pidedirecto.mx>
2024-05-09 20:37:49 -0600

    add translations

---
+++ src/locales/es.ts
@@ -0,0 +1,7 @@
+export const es  = {
+    'Oscar Moreno': 'Óscar Moreno',
+    "Hello! I'm Oscar Moreno, i'm a frontend developer passionated by videogames, movies and languages." : '¡Hola! Soy Óscar Moreno, un desarrollador fontend apasionado por los videojuegos, peliculas e idiomas.',
+    "I consider myself a person with analytic abilities, intuitive and capable of acquire knowledge in a efficient and autodidact way.": 'Me considero una persona con habilidades analíticas, intuitiva y capaz de adquirir conocimiento de una forma eficiente y autodidacta.',
+    'Selfie of my self': 'Selfie de mi mismo',
+    'linkedin icon': 'Icono de linkedin'
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from 'src/App.vue'
+import { createI18n } from 'vue-i18n'
+import { es } from "src/locales/es.ts";
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const i18n = createI18n({
+    locale: navigator.language,
+    fallbackLocale: 'es',
+    messages: {
+        es,
+        en: {
+            Hello: 'Hello'
+        }
+    }
+})
+
+app.use(i18n)
+app.mount('#app')
